40abd4a54c6d87ab60dec3e9cc7585030422424e
Patryk <pludowskipatryk@gmail.com>
2022-10-12 15:33:03 +0200

    Build home page

--- lib/screens/home_page/home_page.dart
+++ lib/screens/home_page/home_page.dart
@@ -1,10 +1,80 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+  const HomePage({
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Welcome in my app'),
+        ),
+        body: Column(
+          children: [
+            Container(
+              margin: const EdgeInsets.all(50),
+              width: 250,
+              child: const Text(
+                'App will help you to learn spelling of english alphabet by solving Quiz where you can hear well know acronyms. ',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                  fontSize: 18.0,
+                ),
+              ),
+            ),
+            const HomePageItem(page: HomePage(), title: 'Quiz Page'),
+            const HomePageItem(page: HomePage(), title: 'Alphabet Page'),
+          ],
+        ));
+  }
+}
+
+class HomePageItem extends StatelessWidget {
+  const HomePageItem({
+    Key? key,
+    required this.page,
+    required this.title,
+  }) : super(key: key);
+  final Widget page;
+  final String title;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        InkWell(
+          onTap: () => Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (_) => page,
+            ),
+          ),
+          child: Container(
+            margin: const EdgeInsets.symmetric(horizontal: 45),
+            height: 80,
+            decoration: BoxDecoration(
+              color: Colors.brown.shade200,
+              borderRadius: const BorderRadius.all(Radius.circular(32)),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  title,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                    fontSize: 18.0,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        const SizedBox(height: 45),
+      ],
+    );
   }
 }
